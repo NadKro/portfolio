79d8985a902f1510cba89ff4041102c60fd503d2
Daniel Tieber <daniel.tieber@redbull.com>
2022-09-09 10:47:08 +0200

    feat(client): load tailwind styles in main component

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,6 +1,8 @@
+import './index.css'
+
 import type { JSX } from 'preact'
 import { render } from 'preact'
 
-const MyComponent = (): JSX.Element => <h1>Hello World!</h1>
+const MyComponent = (): JSX.Element => <h1 className='text-3xl font-bold underline text-red-600'>Hello World!</h1>
 
 render(<MyComponent />, document.body)
