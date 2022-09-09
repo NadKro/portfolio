042af7ee80cab421fd48ad97fac1e021ba3d3c25
Daniel Tieber <daniel.tieber@redbull.com>
2022-09-09 13:01:06 +0200

    test(client): add sample responses from SB for all stories

---
+++ packages/client/src/api/__tests__/storyblok.samples.ts
@@ -0,0 +1,98 @@
+/* eslint-disable */
+
+export const profileResponse = {
+  space: {
+    id: 173751,
+    name: 'nadine-kroepfl.com',
+    domain: 'https://quickstart.me.storyblok.com/',
+    version: 1662715464,
+    language_codes: [],
+  },
+}
+
+export const storiesResponse = {
+  cv: 1662715464,
+  links:
+    [],
+  rels:
+    [],
+  stories:
+    [
+      {
+        alternates:
+          [],
+        content:
+          {
+            _uid: '0d8a6bab-e549-4877-aa1f-524e8bf76b2a',
+            body:
+              [
+                {
+                  _uid: '9d11f96b-8175-43cc-b671-defb0c747baf',
+                  component: 'teaser',
+                  headline: 'Hello world!',
+                },
+                {
+                  _uid: '7ff5b299-b324-4508-8fb4-e79ad6bbed88',
+                  component: 'reference',
+                  logo:
+                    {
+                      alt: null,
+                      copyright: null,
+                      fieldtype: 'asset',
+                      filename: '',
+                      focus: null,
+                      id: null,
+                      name: '',
+                      title: null,
+                    },
+                  title: 'Mozarteum',
+                },
+                {
+                  _uid: '73deff21-2421-423f-b5a5-17b4e792ba8d',
+                  columns:
+                    [
+                      {
+                        _uid: '0379d9bb-b0ef-47e2-8a95-4a6d943b9c94',
+                        component: 'feature',
+                        name: 'Feature 1',
+                      },
+                      {
+                        _uid: '3c283daf-1ca0-47fd-8d7c-036a6aec9684',
+                        component: 'feature',
+                        name: 'Feature 2',
+                      },
+                      {
+                        _uid: '4f3ecae6-cdcc-4128-8fea-6f21220b7f58',
+                        component: 'feature',
+                        name: 'Feature 3',
+                      },
+                    ],
+                  component: 'grid',
+                },
+              ],
+            component: 'page',
+          },
+        created_at: '2022-09-08T13:47:09.477Z',
+        default_full_slug: null,
+        first_published_at: '2022-09-09T09:24:24.180Z',
+        full_slug: 'home',
+        group_id: 'e33627f0-8b3f-4248-9558-3b3f5d7db85d',
+        id: 186250366,
+        is_startpage: false,
+        lang: 'default',
+        meta_data: null,
+        name: 'Home',
+        parent_id: 0,
+        path: null,
+        position: 0,
+        published_at: '2022-09-09T09:24:24.180Z',
+        release_id: null,
+        slug: 'home',
+        sort_by_date: null,
+        tag_list:
+          [],
+        translated_slugs: null,
+        uuid: '44396e6c-8a16-4b30-818e-d237fe48fa2b',
+      },
+    ],
+}
