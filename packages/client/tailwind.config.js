5d08f627c5d2c66191de0d07dd9039de8a924137
Daniel Tieber <daniel.tieber@redbull.com>
2022-09-15 15:11:07 +0200

    build(client): use `class` strategy to toggle dark mode
    https://tailwindcss.com/docs/dark-mode

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
